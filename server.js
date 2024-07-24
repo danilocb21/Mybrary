5217e2ea6bddc60d8de30912309e0097f4f4913a
Danilo <danilocbjp@hotmail.com>
2024-07-24 10:25:38 -0300

    feat: increase limit of file upload to 5 mb

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -15,8 +16,8 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use(express.json({ limit: "5mb" }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
